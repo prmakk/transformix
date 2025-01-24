b2fd32366b74d62ea715e9ee62821bfa04a814b5
unknown <priymakvlad420@gmail.com>
2025-01-24 19:55:21 +0200

    chore:init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/main.css";
+
+function App() {
+    return (
+        <div className="bg-sky-200">
+            <h1 className="text-9xl">test</h1>
+        </div>
+    );
+}
+
+export default App;
